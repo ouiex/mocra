cba6a040d36c20bbc981aceaf7de9bd7d5f36091
eason <hanclong@hotmail.com>
2026-01-07 22:06:39 +0800

    bug fix

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,21 +1,6 @@
-mod engine;
-mod api;
-mod chain;
-mod events;
-mod processors;
-mod task;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod engine;
+pub mod api;
+pub mod chain;
+pub mod events;
+pub mod processors;
+pub mod task;
