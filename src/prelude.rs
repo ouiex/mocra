b6d6bce507b75a27d19b8a244627a4ece03b1d92
eason <hanclong@hotmail.com>
2026-01-07 22:30:30 +0800

    add prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,24 @@
+// Common Traits and Structs
+pub use ::common::interface::{
+    DataMiddleware, DataStoreMiddleware, DownloadMiddleware, MiddlewareManager, ModuleNodeTrait,
+    ModuleTrait, StoreTrait, SyncBoxStream,
+};
+pub use ::common::model::data::Data;
+pub use ::common::model::login_info::LoginInfo;
+pub use ::common::model::message::{ParserData, ParserTaskModel};
+pub use ::common::model::request::RequestMethod;
+pub use ::common::model::{
+    Cookies, ExecutionMark, Headers, ModuleConfig, Request, Response,
+};
+
+// Errors
+pub use ::errors::{
+    BoxError, Error, ErrorKind, ParserError, RequestError, Result,
+};
+
+// Utils
+pub use ::utils::date_utils::DateUtils;
+
 pub mod common {
     pub use ::common::interface::DataMiddleware;
     pub use ::common::interface::DataStoreMiddleware;
@@ -54,6 +75,8 @@ pub mod errors {
     pub use ::errors::BoxError;
     pub use ::errors::Error;
     pub use ::errors::ErrorKind;
+    pub use ::errors::ParserError;
+    pub use ::errors::RequestError;
     pub use ::errors::Result;
 }
 pub mod cacheable {
